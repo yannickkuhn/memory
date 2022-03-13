6cc0aea099693ecab36f66a26eb29a8e3f4a1753
Yannick KUHN <yannickkuhn06@gmail.com>
2022-03-13 02:29:46 +0100

    Premier tri effectué des fruits en JQuery

--- js/memory.js
+++ js/memory.js
@@ -1,12 +1,28 @@
+// Algorithme de tri aléatoire selon Fisher Yates
+function fisherYatesShuffle(arr){
+    for(var i = arr.length-1; i>0 ;i--) {
+        var j = Math.floor( Math.random() * (i + 1) ); // calcul aléatoire d'une case
+        [arr[i],arr[j]]=[arr[j],arr[i]]; // échange des valeurs du tableau
+    }
+}
+
 $(document).ready(function() {
     $(".fruit").hide();
     $(".empty_color").show();
 
-    // Ajout du fruit numéro 1 sur chaque fruit
+    // le tableau réprésente les fruits sur le plateau de jeux
+    let fruits = [1, 2, 3, 4, 5, 6, 7, 8, 
+                  1, 2, 3, 4, 5, 6, 7, 8];
+
+    // on trie de façon aléatoire tous les fruits avant de les affecter aux éléments de type card (html)
+    fisherYatesShuffle(fruits);
+
+    // Ajout d'un fruit du tableau sur chaque élément card
     $(".fruit").each(function(index) {
-        $(this).addClass("fruit1");
+        $(this).addClass("fruit" + fruits[index]);
     })
 
+    // Action lors d'un clic sur un élement card
     $(".card").click(function() {
         $(this).find(".empty_color").toggle();
         $(this).find(".fruit").toggle();
